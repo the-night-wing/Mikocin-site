7e94e4d789d866b1ebc6aa3449a08bee4917f63b
Den <denisanatoliyovich@gmail.com>
2020-07-22 18:56:45 +0300

    Add 'build' task for minifying

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('styles', () => {
   return gulp
-    .src('styles/*.scss')
+    .src('styles/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./styles/'));
 });
@@ -18,5 +21,28 @@ gulp.task('watch', () => {
     gulp.series(['clean', 'styles'])(done);
   });
 });
+
+gulp.task('minifyCSS', () => {
+  return gulp
+    .src('styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(gulp.dest('./styles/'));
+});
+
+gulp.task('minifyHTML', () => {
+  return gulp
+    .src(['./index.html'])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true
+      })
+    )
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', gulp.series(['minifyCSS', 'minifyHTML']));
 
 gulp.task('default', gulp.series(['clean', 'styles']));
